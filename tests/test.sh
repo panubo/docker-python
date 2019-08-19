76c57de8488f84b7907821b256ece21a3ab2ab8e
Andrew Cutler <andrew@panubo.com>
2019-08-19 11:04:00 +1000

    make build noisy in CI

--- tests/test.sh
+++ tests/test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 # Build and test all the containers.
+# NOBUILD = pull and test the containers
+# NB: Does a noisy build in CI environments
 
 set -e
 [ "${DEBUG}" == 'true' ] && set -x
@@ -9,7 +11,11 @@ function build_container {
   VER="$1"
   pushd ${VER} 1> /dev/null
   echo ">> Building ${VER}"
-  docker build --quiet -t panubo/python:${VER}-test .
+  if [ "${CI}" == "true" ]; then
+    docker build -t panubo/python:${VER}-test .
+  else
+    docker build --quiet -t panubo/python:${VER}-test .
+  fi
   popd 1> /dev/null
 }
 
@@ -21,13 +27,13 @@ function pull_container {
 }
 
 for v in 2.7 3.2 3.3 3.4 3.5 3.6 3.7; do
-  [ "${TRAVIS}" == 'true' ] && pull_container ${v} || build_container ${v}
+  [ "${NOBUILD}" == 'true' ] && pull_container ${v} || build_container ${v}
   OUTPUT="$(docker run --rm -ti panubo/python:${v}-test python${v} --version)"
   [[ "${OUTPUT}" == *"Python ${v}"* ]] || { echo "Version test failed. $OUTPUT"; exit 128; } && { echo "> $v OK."; }
 done
 
 # All versions test
-[ "${TRAVIS}" == 'true' ] && pull_container all || build_container all
+[ "${NOBUILD}" == 'true' ] && pull_container all || build_container all
 for v in 2.7 3.3 3.4 3.5 3.6 3.7; do
   OUTPUT=$(docker run --rm -ti panubo/python:all-test python${v} --version)
   [[ "${OUTPUT}" == *"Python ${v}"* ]] || { echo "Version test failed. $OUTPUT"; exit 128; } && { echo "> $v OK."; }
